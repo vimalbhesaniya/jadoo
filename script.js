cec203aff682334b1a79f262c5a63a6df2310b59
vimalbhesaniya <vimalbhesaniya007@gmail.com>
2024-04-22 00:08:00 +0530

    --work in progress

--- script.js
+++ script.js
@@ -146,17 +146,17 @@ function changeText() {
         three.classList.remove("activeDot")
         one.classList.remove("activeDot")
         document.getElementById("sliderText").innerHTML = "What people say about us?"
-        one.classList.add("activeDot")
+        two.classList.add("activeDot")
     } , 5000);
     setTimeout(()=>{
-        one.classList.remove("activeDot")
+        two.classList.remove("activeDot")
         document.getElementById("sliderText").innerHTML = "Why People Like Us?"
-        two.classList.add("activeDot")
+        three.classList.add("activeDot")
     } , 10000);
     setTimeout(()=>{
-        two.classList.remove("activeDot")
+        three.classList.remove("activeDot")
         document.getElementById("sliderText").innerHTML = "Is There Any problem?"
-        three.classList.add("activeDot")
+        one.classList.add("activeDot")
         changeText()
     } , 15000);
 }
